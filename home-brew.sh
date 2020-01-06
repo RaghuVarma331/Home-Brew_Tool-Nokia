e54b0094707de7720a9c83fc550e1cd60baad0c0
Raghu Varma <raghuvarma331@gmail.com>
2020-01-06 18:52:54 +0530

    Add Android 10 DRG410C & CTL 408B OTA Links

--- home-brew.sh
+++ home-brew.sh
@@ -21,10 +21,10 @@ clear
 # Init Fields
 
 Telegram_Api_code=
-DRG=https://android.googleapis.com/packages/ota-api/nokia_drgsprout_dragon00ww/810351d123009ec07c1cb5857c4707fdeba776ef.zip
+DRG=https://android.googleapis.com/packages/ota-api/nokia_drgsprout_dragon00ww/07330e258c1095c5eea9e1b697bb4ea8d92ebc9e.zip
 B2N=https://android.googleapis.com/packages/ota-api/nokia_b2nsprout_onyx00ww/3f78de06f86cc03da028648341aa1987fe33b2df.zip
 PL2=https://android.googleapis.com/packages/ota-api/nokia_pl2sprout_plate200ww/be7e54518410d1752dd72f99dba3aecdee793bdb.zip
-CTL=https://android.googleapis.com/packages/ota-api/nokia_ctlsprout_crystal00ww/b708f828b774c5d737530f3d1ea200db7a918308.zip
+CTL=https://android.googleapis.com/packages/ota-api/nokia_ctlsprout_crystal00ww/29ad191f90eedc643ed008dc06c81ed3c5b5565a.zip
 DDV=https://android.googleapis.com/packages/ota-api/nokia_ddvsprout_daredevil00ww/833c052838239419faa9a4510da474c8b0caf7e3.zip
 SLD=https://android.googleapis.com/packages/ota-api/nokia_sldsprout_starlord00ww/7aba315391c8a45bc23f809ef27e072086d4c8d3.zip
 password=
